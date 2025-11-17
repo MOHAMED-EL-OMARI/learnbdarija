c30d925bc60e781915d703db40b049bde0b11d58
MOHAMED EL OMARI <mohzm.gjk63@gmail.com>
2025-11-17 02:04:17 +0100

    initialize the project and created the migrations

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header>
+        <h1 class="h1">Welcome to Learn B Darija</h1>
+    </header>
+    <main>
+        <p>This is the welcome page of the Learn B Darija application.</p>
+    </main>
+    <footer>
+        <p>&copy; 2024 Learn B Darija</p>
+    </footer>
+</body>
+</html>
